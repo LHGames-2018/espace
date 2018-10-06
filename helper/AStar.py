8d60fbaa61170e63bc716b015d69882f6bf9f02a
david.sylvestre27@gmail.com <david.sylvestre.1@ens.etsmtl.ca>
2018-10-06 15:10:31 -0400

    A* now returns weight

--- helper/AStar.py
+++ helper/AStar.py
@@ -36,7 +36,7 @@ def aStar(maze, start, goal, weightSymbols):
             while cameFrom[point] != start:
                 point = cameFrom[point]
  
-            return [x-y for x,y in zip(point, cameFrom[point])]
+            return (currentNode.gscore, tuple([x-y for x,y in zip(point, cameFrom[point])]))
             
 
         closedlist.append(currentNode.point)
@@ -52,7 +52,7 @@ def aStar(maze, start, goal, weightSymbols):
             if symbolWeight == -1:
                 continue
 
-            newNode = Node(newPoint, currentNode.weight + symbolWeight, heuristic(newPoint, goal))
+            newNode = Node(newPoint, currentNode.gscore + symbolWeight, heuristic(newPoint, goal))
 
             if sum(1 for x in openlist if x.point == newPoint) == 0:
                 heapq.heappush(openlist, newNode)
